ed2674975653a157b4fccbad0768687abb164e72
cxmorris <66590011+cxmorris@users.noreply.github.com>
2023-04-16 15:51:30 +0200

    initial files

---
+++ main.py
@@ -0,0 +1,11 @@
+import pandas as pd
+
+print("hello")
+df = pd.read_csv('Iris.csv')
+print(df.head())
+
+#created a venv and installed pandas, jupyter, 
+# seaborn, scikit-learn, keras, tensorflow
+
+#following this tutorial
+#https://code.visualstudio.com/docs/datascience/data-science-tutorial
